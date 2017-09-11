d6fd9fbb46253801f554a5d44d960587507eed2e
kafrax <kafrax.go@gmail.com>
2017-09-11 12:08:16 +0800

    update

--- validator.go
+++ validator.go
@@ -9,6 +9,8 @@ var (
 	V_REGEXP_USERNAME = "^[a-zA-Z0-9_]{4,16}$"
 	V_REGEXP_PASSWORD = "^[a-zA-Z0-9]{6,16}$"
 	V_REGEXP_NICK     = "^[\u4E00-\u9FA5A-Za-z0-9_]{2,12}$"
+	V_REGEXP_EMAIL    = "[\\w!#$%&'*+/=?^_`{|}~-]+(?:\\.[\\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\\w](?:[\\w-]*[\\w])?\\.)+[\\w](?:[\\w-]*[\\w])?"
+	V_REGEXP_CHINESE  = "^[\\u4e00-\\u9fa5]{0,}$"
 )
 //validator string、phone、email etc.
 func IsPhone(phone string) bool {
@@ -24,6 +26,32 @@ func IsUserName(userName string) bool {
 func IsNick(nick string) bool {
 	reg := regexp.MustCompile(V_REGEXP_NICK)
 	return reg.MatchString(nick)
+}
+
+func IsEmail(mail string) bool {
+	reg := regexp.MustCompile(V_REGEXP_EMAIL)
+	return reg.MatchString(mail)
+}
+
+func IsChinese(chars string) bool {
+	reg := regexp.MustCompile(V_REGEXP_CHINESE)
+	return reg.MatchString(chars)
+}
+
+func IsNilString(s string) bool {
+	if len(s) < 1 {
+		return true
+	}
+	return false
+}
+
+func IsAllNilString(s ...string) bool {
+	for _, v := range s {
+		if len(v) > 1 {
+			return false
+		}
+	}
+	return true
 }
 
 func IsPassword(pwd string) bool {
