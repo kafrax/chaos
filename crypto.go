4aefbff0aca4ef37f9fc79fab0ceed975b39fff9
kafrax <kafrax.go@gmail.com>
2017-09-11 11:44:26 +0800

    simple crypto use

--- crypto.go
+++ crypto.go
@@ -1,3 +1,47 @@
 package chaos
 
+import (
+	"io"
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"crypto/sha1"
+)
+
 //commonly used secret function
+
+func MD5BySalt(src,salt string)string{
+	hash := md5.New()
+	io.WriteString(hash, src)
+	io.WriteString(hash, salt)
+	return hex.EncodeToString(hash.Sum(nil))
+}
+
+func MD5(src string)string{
+	hash := md5.New()
+	io.WriteString(hash, src)
+	return hex.EncodeToString(hash.Sum(nil))
+}
+
+func SHA1BySalt(src,salt string)(string,error){
+	t := sha1.New()
+	_, err := io.WriteString(t, src)
+	if err != nil {
+		return "",err
+	}
+	_,err =io.WriteString(t,salt)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%x", t.Sum(nil)), nil
+}
+
+func SHA1(src  string)(string,error){
+	t := sha1.New()
+	_, err := io.WriteString(t, src)
+	if err != nil {
+		return "",err
+	}
+	return fmt.Sprintf("%x", t.Sum(nil)), nil
+}
+
