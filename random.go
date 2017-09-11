ba284ae9d0f69714f96ddf393bf4dd39f16b766f
kafrax <kafrax.go@gmail.com>
2017-09-11 11:35:56 +0800

    add use uuid+unix time for user_id and so on

--- random.go
+++ random.go
@@ -1,3 +1,41 @@
 package chaos
 
-//get random string
+import (
+	"strconv"
+	"time"
+	"io"
+	"crypto/rand"
+
+	"github.com/google/uuid"
+)
+
+//uuid+unix time
+func RandUserId() string {
+	return strconv.FormatInt(int64(uuid.New().Time()/10000000000)*10000000000+time.Now().Unix(), 10)
+}
+
+//0123456789 select 6 password number
+const C_RAND_TMP= "0123456789"
+func RandPassword(length int, chars []byte) string {
+	newPwd := make([]byte, length)
+	random := make([]byte, length+(length/4)) // storage for random bytes.
+	charsLength := byte(len(chars))
+	max := byte(256 - (256 % len(chars)))
+	i := 0
+	for {
+		if _, err := io.ReadFull(rand.Reader, random); err != nil {
+			panic(err)
+		}
+		for _, c := range random {
+			if c >= max {
+				continue
+			}
+			newPwd[i] = chars[c%charsLength]
+			i++
+			if i == length {
+				return string(newPwd)
+			}
+		}
+	}
+	panic("unreachable")
+}
